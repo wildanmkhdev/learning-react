cadda092dc2b031ff8f86dfe747b9238dfa0ba91
wildanmkhdev <wildankhalidun97@gmail.com>
2025-08-14 01:18:19 +0700

    update

--- src/state/BelajarState.tsx
+++ src/state/BelajarState.tsx
@@ -3,11 +3,13 @@ const BelajarState = () => {
 		<div>
 			<div>
 				<p className="text-blue-700 text-center text-2xl">
-					hasi hari ini kita akan belajar state managemebt di react
+					hasi hari ini kita akan belajar state managemet di reactz
 				</p>
 			</div>
 		</div>
+
 	);
 };
 
 export default BelajarState;
+
